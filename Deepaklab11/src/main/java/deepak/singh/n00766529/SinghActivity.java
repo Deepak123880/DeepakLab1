49b6ca7d57f1220cac4e77490322624140f75c62
Deepak <sngd0512@humbermail.ca>
2021-01-28 22:08:37 -0500

    initial commit of my 2 created activities for deepak and singh activity

---
+++ Deepaklab11/src/main/java/deepak/singh/n00766529/SinghActivity.java
@@ -0,0 +1,17 @@
+/*
+Deepak Singh n00766529 section A
+ */
+package deepak.singh.n00766529;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SinghActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singh);
+    }
+}
